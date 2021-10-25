f60def39d47b4a479e7196eb2734d4eeb4e9d36c
Kazeem Quadri <quadrikazeem01@gmail.com>
2021-10-25 10:52:19 +0100

    Change Project type from Razor Page to MVC

---
+++ ELibraryWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ELibrary.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
