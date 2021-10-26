7d388763621b1045f973ccbe6a778bbad4e55d57
Kazeem Quadri <quadrikazeem01@gmail.com>
2021-10-26 08:00:36 +0100

    Admin area

---
+++ ELibrary/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ELibrary.Areas.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
