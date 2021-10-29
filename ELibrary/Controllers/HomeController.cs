bcbca64953563f350d79ac51c4d5c7870cb86752
Kazeem Quadri <quadrikazeem01@gmail.com>
2021-10-29 23:49:53 +0100

    Description Page

--- ELibrary/Controllers/HomeController.cs
+++ ELibrary/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using ELibrary.Models;
+﻿using AutoMapper;
+using ELibrary.Models;
+using ELibrary.Service.Contract;
+using ELibrary.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
@@ -8,15 +11,35 @@ namespace ELibrary.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IRepositoryServiceManager _repositoryService;
+        private readonly IMapper _mapper;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IRepositoryServiceManager repositoryService, IMapper mapper)
         {
             _logger = logger;
+            _repositoryService = repositoryService;
+            _mapper = mapper;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var books = _repositoryService.BookService.GetAllBooks(true).ToList();
+            var _books = _mapper.Map<List<BookViewModel>>(books);
+            HomeViewModel model = new();
+            model.books = _books;
+            return View(model);
+        }
+
+        [Route("{id}")]
+        public IActionResult Detail(int id)
+        {
+            var model = _repositoryService.BookService.GetById(id, true);
+            if (model == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            var book = _mapper.Map<BookViewModel>(model);
+            return View(book);
         }
 
         public IActionResult Privacy()
