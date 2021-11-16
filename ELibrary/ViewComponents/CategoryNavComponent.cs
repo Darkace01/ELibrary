afd95c5edfb392fb86030731f275fb5653a00984
Kazeem Quadri <quadrikazeem01@gmail.com>
2021-11-16 14:44:46 +0100

    Category Nav bar view component

---
+++ ELibrary/ViewComponents/CategoryNavComponent.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using ELibrary.Service.Contract;
+using ELibrary.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ELibrary.ViewComponents;
+
+[ViewComponent(Name = "CategoryNav")]
+public class CategoryNavComponent : ViewComponent
+{
+    private readonly IRepositoryServiceManager _repositoryService;
+    private readonly IMapper _mapper;
+
+    public CategoryNavComponent(IRepositoryServiceManager repositoryService, IMapper mapper)
+    {
+        _repositoryService = repositoryService;
+        _mapper = mapper;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var category = _mapper.Map<List<CategoryViewModel>>(_repositoryService.CategoryService.GetAll().ToList());
+        return View(category);
+    }
+}
