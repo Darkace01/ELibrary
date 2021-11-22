42503960dd1d33e52ced4436daaa7d313428d86d
Kazeem Quadri <quadrikazeem01@gmail.com>
2021-11-22 14:09:10 +0100

    Related Books

---
+++ ELibrary/ViewComponents/RelatedBooksComponent.cs
@@ -0,0 +1,19 @@
+﻿namespace ELibrary.ViewComponents;
+[ViewComponent(Name = "RelatedBooks")]
+public class RelatedBooksComponent :  ViewComponent
+{
+    private readonly IRepositoryServiceManager _repositoryService;
+    private readonly IMapper _mapper;
+
+    public RelatedBooksComponent(IRepositoryServiceManager repositoryService, IMapper mapper)
+    {
+        _repositoryService = repositoryService;
+        _mapper = mapper;
+    }
+
+    public IViewComponentResult Invoke(int bookId)
+    {
+        var relatedBooks = _mapper.Map<List<BookViewModel>>(_repositoryService.BookService.GetAllBooks(true).Where(x => x.Id != bookId).Take(6).OrderBy(x => x.Name).ToList());
+        return View(relatedBooks);
+    }
+}
