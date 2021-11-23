d9bcbe340f47dd7d07b60452f0ba70c85b4fea63
Kazeem Quadri <quadrikazeem01@gmail.com>
2021-11-23 13:46:59 +0100

    Pdf extension check

--- ELibrary.Utility/CommonHelper.cs
+++ ELibrary.Utility/CommonHelper.cs
@@ -1,11 +1,12 @@
-﻿namespace ELibrary.Utility;
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ELibrary.Utility;
 
 public class CommonHelper
 {
-    public static List<string> ConvertStringToList(string strings, char seperator)
-    {
-        if (string.IsNullOrEmpty(strings) || string.IsNullOrWhiteSpace(strings))
-            return new List<string>();
-        return strings.Split(seperator).ToList();
-    }
+    public static List<string> ConvertStringToList(string strings, char seperator) => string.IsNullOrEmpty(strings) || string.IsNullOrWhiteSpace(strings) ? new List<string>() : strings.Split(seperator).ToList();
+
+    public static string GetFileFormat(IFormFile file) => new FileInfo(file.FileName).Extension;
+
+    public static bool CheckFileFormat(IFormFile file, string extension) => GetFileFormat(file).ToLower() == extension.ToLower();
 }
